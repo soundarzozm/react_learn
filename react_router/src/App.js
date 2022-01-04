c5ea69e07eaface7145af44d3eae230fe33ae0df
soundarzozm <soundar.murugan1@learner.manipal.edu>
2022-01-04 15:16:58 +0530

    react-router-dom new version till linking

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import Post from './components/posts';
+import Profile from './components/profile';
+import Home from './components/home';
 
 const App = () => {
   return (
-    <>
-      Hello World!
-    </>
+    <BrowserRouter>
+      <header>
+        <Link to="/profile">Profile</Link>
+      </header>
+      <Routes>
+        <Route path="/posts" element={<Post/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
