5cd28374aa88c39f6d3a7f4a117e6750bf262220
soundarzozm <soundar.murugan1@learner.manipal.edu>
2022-01-07 10:09:59 +0530

    downgraded reactrouter to 5

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 import Post from './components/posts';
 import Profile from './components/profile';
 import Home from './components/home';
@@ -7,14 +7,30 @@ import Home from './components/home';
 const App = () => {
   return (
     <BrowserRouter>
+
       <header>
-        <Link to="/profile">Profile</Link>
+
+        <Link to={{
+          pathname: "/profile",
+          hash: "#francis",
+          search: "?true-enabled"
+        }}>Profile</Link>
+        <br/>
+
+        <Link to="/">Home</Link>
+        <br/>
+
+        <Link to="/posts">Posts</Link>
+        <br/>
+
+        <hr/>
+
       </header>
-      <Routes>
-        <Route path="/posts" element={<Post/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
+
+        <Route path="/posts" component={Post}/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/profile" exact component={Profile}/>
+      
     </BrowserRouter>
   );
 }
--- react_router/src/components/profile.js
+++ react_router/src/components/profile.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Profile = () => {
+const Profile = (props) => {
+
+  console.log(props);
+
   return (
     <>
-      Profile
+      <Link to={{
+        pathname: `${props.match.url}/posts`
+      }}>
+        posts of profile
+      </Link>
     </>
   );
 }
