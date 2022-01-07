be51a8cb36d530baf631ac3ed192c8cdeefa31d0
soundarzozm <soundar.murugan1@learner.manipal.edu>
2022-01-07 11:47:11 +0530

    NavLink and different routers

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+// MemoryRouter, HashRouter
 
 import Home from './components/home';
 import Post from './components/posts';
@@ -10,26 +11,38 @@ const App = () => {
   return (
     <BrowserRouter>
         <header>
-          <div className="d-flex flex-column flex-md-row align-items-center p-3 
-          px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+          
+          <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+            
+            <h5 className="my-0 mr-md-auto font-weight-normal">
+              Company name
+            </h5>
+            
             <nav className="my-2 my-md-0 mr-md-3">
-              <Link className="p-2 text-dark" to="/">Home</Link> -
-              <Link className="p-2 text-dark" to="/posts">Posts</Link> - 
-              <Link className="p-2 text-dark" to={{
+              
+              <NavLink className="p-2" activeStyle={{color: 'red'}} activeClassName='selected' exact to="/">Home</NavLink> -
+              
+              <NavLink className="p-2" activeStyle={{color: 'red'}} activeClassName='selected' to="/posts">Posts</NavLink> - 
+              
+              <NavLink className="p-2" activeStyle={{color: 'red'}} activeClassName='selected' to={{
                 pathname:'/profile',
                 hash:'#francis',
                 search:'?true=enabled'
-              }}>Profile</Link>
+              }}>Profile</NavLink>
+
             </nav>
+          
           </div>
+        
         </header>
+        
         <div className="container">
           <Route path="/" exact component={Home}/>
           <Route path="/posts" exact component={Post}/>
           <Route path="/posts/:id" exact component={PostItem}/>
           <Route path="/profile" exact component={Profile}/>
         </div>
+
     </BrowserRouter>
   );
 }
