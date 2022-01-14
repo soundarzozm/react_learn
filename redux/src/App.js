115dfe0edf156433f771aa0e7df84b9dea006e76
soundarzozm <soundar.murugan1@learner.manipal.edu>
2022-01-14 10:51:07 +0530

    redux template

---
+++ redux/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/header';
+import Home from './components/home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
